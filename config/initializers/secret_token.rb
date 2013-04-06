b65b5d77025f4aaf3b010b010e1448228a1a4e7b
XEdP3X <mario@xedp3x.de>
2013-04-06 11:22:16 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pam::Application.config.secret_token = '0005b454628f3c7a6de619ad3dc6f2b68f3b23413ca68741cdb097091190ba41c368a1b14d92a9dd180638c3e2a689d262e9eb3b17686ca4e5a0fe39a9bfb58a'
