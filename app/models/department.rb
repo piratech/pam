91925f42da66bfcc3fb49d9e0029653ddf20f8c1
XEdP3X <mario@xedp3x.de>
2013-04-07 14:08:56 +0200

    relation ob entrys

--- app/models/department.rb
+++ app/models/department.rb
@@ -2,4 +2,14 @@ class Department < ActiveLdap::Base
   ldap_mapping dn_attribute: "cn",
                prefix: "ou=Departments",
                classes: ["inetOrgPerson"]
+  belongs_to :persons, :many => 'o'
+
+  alias groups_mapping persons
+  def persons(return_objects=true)
+    return groups_mapping if return_objects
+    attr = 'cn'
+    Person.search(:attribute => 'o',
+    :value => id,
+    :attributes => [attr]).map {|dn, attrs| attrs[attr]}.flatten
+  end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,19 @@
 class Person < ActiveLdap::Base
-  ldap_mapping dn_attribute: "cn",
+  ldap_mapping dn_attribute: "uid",
                prefix: "ou=People",
                classes: ["inetOrgPerson", "PosixAccount","top"]
+
+  belongs_to :primary_group, :class_name => "Group",
+             :foreign_key => "gidNumber", :primary_key => "gidNumber"
+  belongs_to :groups, :many => 'memberUid'
+  has_many   :departments, :class_name => "Departments", :wrap => "o"
+
+  alias groups_mapping groups
+  def groups(return_objects=true)
+    return groups_mapping if return_objects
+    attr = 'cn'
+    Group.search(:attribute => 'memberUid',
+    :value => id,
+    :attributes => [attr]).map {|dn, attrs| attrs[attr]}.flatten
+  end
 end
